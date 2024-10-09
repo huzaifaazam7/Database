bfd34461c21205ab5782c53dfc897bce207ae3e1
huzaifaazam7 <muhammadhuzaifaazam7@gmail.com>
2024-10-09 17:02:34 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+// pages
+import Home from "./pages/Home"
+import Create from "./pages/Create"
+import Update from "./pages/Update"
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <h1>Best Social Media app! </h1>
+        <Link to="/">See the Response</Link>
+        <Link to="/create">Fill Your Chiose</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
